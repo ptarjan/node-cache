99420e3eee19da0e901d4cb474b659f4c53acaf5
Shan Thomas <vxst@vxst.org>
2013-05-13 13:13:01 +0800

    Update index.js

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ exports.put = function(key, value, time, timeoutCallback) {
 exports.del = function(key) {
   if (typeof cache[key] === 'undefined') {
     return false;
-  } else if (  isNaN(data.expire) || data.expire >= now() ){
+  } else if (isNaN(data.expire) || data.expire >= now()){
     delete cache[key];
     return true;
   } else {
