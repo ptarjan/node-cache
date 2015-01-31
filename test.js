0d853bc5c7ada4e96457ae933b51c9a948c62247
delong <w.del@qq.com>
2015-01-31 10:09:22 +0800

    format
    just use IDEA format files

--- test.js
+++ test.js
@@ -1,55 +1,54 @@
-var cache = require('./index')
-;
+var cache = require('./index');
 
 cache.debug(false);
 
 cache.put('a', true);
-console.log('true == '+cache.get('a'));
+console.log('true == ' + cache.get('a'));
 cache.clear();
-console.log('null == '+cache.get('a'));
+console.log('null == ' + cache.get('a'));
 
-console.log('null == '+cache.get('a'));
-console.log('0 == '+cache.size());
+console.log('null == ' + cache.get('a'));
+console.log('0 == ' + cache.size());
 
 cache.put('a', 'b', 3000);
-console.log('1 == '+cache.size());
+console.log('1 == ' + cache.size());
 
-console.log('b == '+cache.get('a'));
+console.log('b == ' + cache.get('a'));
 
 console.log("keys == ", cache.keys());
 
-var complicated = ['a',{'b':'c','d':['e',3]},'@'];
+var complicated = ['a', {'b': 'c', 'd': ['e', 3]}, '@'];
 cache.put(complicated, true);
-console.log('true == '+cache.get(complicated));
+console.log('true == ' + cache.get(complicated));
 cache.del(complicated);
-console.log('null == '+cache.get(complicated));
+console.log('null == ' + cache.get(complicated));
 
-console.log('1 == '+cache.size());
+console.log('1 == ' + cache.size());
 cache.put(0, 0);
-console.log('2 == '+cache.size());
+console.log('2 == ' + cache.size());
 cache.del(0);
 
-setTimeout(function() {
-  console.log('b == '+cache.get('a'));
+setTimeout(function () {
+    console.log('b == ' + cache.get('a'));
 }, 2000);
 
-setTimeout(function() {
-  console.log('null == '+cache.get('a'));
-  console.log('0 == '+cache.size());
+setTimeout(function () {
+    console.log('null == ' + cache.get('a'));
+    console.log('0 == ' + cache.size());
 }, 4000);
 
-setTimeout(function() {
-	console.log('Cache hits: ' + cache.hits());
-	console.log('Cache misses: ' + cache.misses());
+setTimeout(function () {
+    console.log('Cache hits: ' + cache.hits());
+    console.log('Cache misses: ' + cache.misses());
 }, 5000);
 
 cache.put('timeout', 'timeout', 2000);
 
-setTimeout(function() {
-	console.log('timeout == '+cache.get('timeout'));
-	cache.put('timeout', 'timeout-re', 2000); // Cancel timeout on NEW put
+setTimeout(function () {
+    console.log('timeout == ' + cache.get('timeout'));
+    cache.put('timeout', 'timeout-re', 2000); // Cancel timeout on NEW put
 }, 1000);
 
-setTimeout(function() {
-	console.log('timeout-re == '+cache.get('timeout'));
+setTimeout(function () {
+    console.log('timeout-re == ' + cache.get('timeout'));
 }, 3000);
