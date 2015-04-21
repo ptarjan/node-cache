1959acd53a3ee57d4c2366b5c38c96ccb8c4d20e
Olivier Lalonde <olalonde@gmail.com>
2015-04-21 11:00:16 +0800

    Added optional createStore() method.

--- index.js
+++ index.js
@@ -1,120 +1,128 @@
 'use strict';
 
-var cache = Object.create(null);
-var debug = false;
-var hitCount = 0;
-var missCount = 0;
-var size = 0;
+function createStore () {
+  var store = {};
+  var cache = Object.create(null);
+  var debug = false;
+  var hitCount = 0;
+  var missCount = 0;
+  var size = 0;
 
-exports.put = function(key, value, time, timeoutCallback) {
-  if (debug) {
-    console.log('caching: %s = %j (@%s)', key, value, time);
-  }
-  var oldRecord = cache[key];
-  if (oldRecord) {
-    clearTimeout(oldRecord.timeout);
-  } else {
-    size++;
-  }
-
-  var expire = time + Date.now();
-  var record = {
-    value: value,
-    expire: expire
-  };
-
-  if (!isNaN(expire)) {
-    var timeout = setTimeout(function() {
-      exports.del(key);
-      if (typeof timeoutCallback === 'function') {
-        timeoutCallback(key);
-      }
-    }, time);
-    record.timeout = timeout;
-  }
-
-  cache[key] = record;
-};
-
-exports.del = function(key) {
-  var canDelete = true;
-
-  var oldRecord = cache[key];
-  if (oldRecord) {
-    clearTimeout(oldRecord.timeout);
-    if (!isNaN(oldRecord.expire) && oldRecord.expire < Date.now()) {
-      canDelete = false;
+  store.put = function(key, value, time, timeoutCallback) {
+    if (debug) {
+      console.log('caching: %s = %j (@%s)', key, value, time);
     }
-  } else {
-    canDelete = false;
-  }
-
-  if (canDelete) {
-    size--;
-    delete cache[key];
-  }
-
-  return canDelete;
-};
-
-exports.clear = function() {
-  for (var key in cache) {
     var oldRecord = cache[key];
     if (oldRecord) {
       clearTimeout(oldRecord.timeout);
-    }
-  }
-  size = 0;
-  cache = Object.create(null);
-  if (debug) {
-    hitCount = 0;
-    missCount = 0;
-  }
-};
-
-exports.get = function(key) {
-  var data = cache[key];
-  if (typeof data != "undefined") {
-    if (isNaN(data.expire) || data.expire >= Date.now()) {
-      if (debug) hitCount++;
-      return data.value;
     } else {
-      // free some space
-      if (debug) missCount++;
+      size++;
+    }
+
+    var expire = time + Date.now();
+    var record = {
+      value: value,
+      expire: expire
+    };
+
+    if (!isNaN(expire)) {
+      var timeout = setTimeout(function() {
+        store.del(key);
+        if (typeof timeoutCallback === 'function') {
+          timeoutCallback(key);
+        }
+      }, time);
+      record.timeout = timeout;
+    }
+
+    cache[key] = record;
+  };
+
+  store.del = function(key) {
+    var canDelete = true;
+
+    var oldRecord = cache[key];
+    if (oldRecord) {
+      clearTimeout(oldRecord.timeout);
+      if (!isNaN(oldRecord.expire) && oldRecord.expire < Date.now()) {
+        canDelete = false;
+      }
+    } else {
+      canDelete = false;
+    }
+
+    if (canDelete) {
       size--;
       delete cache[key];
     }
-  } else if (debug) {
-    missCount++;
-  }
-  return null;
-};
 
-exports.size = function() {
-  return size;
-};
+    return canDelete;
+  };
 
-exports.memsize = function() {
-  var size = 0,
-    key;
-  for (key in cache) {
-    size++;
-  }
-  return size;
-};
+  store.clear = function() {
+    for (var key in cache) {
+      var oldRecord = cache[key];
+      if (oldRecord) {
+        clearTimeout(oldRecord.timeout);
+      }
+    }
+    size = 0;
+    cache = Object.create(null);
+    if (debug) {
+      hitCount = 0;
+      missCount = 0;
+    }
+  };
 
-exports.debug = function(bool) {
-  debug = bool;
-};
+  store.get = function(key) {
+    var data = cache[key];
+    if (typeof data != "undefined") {
+      if (isNaN(data.expire) || data.expire >= Date.now()) {
+        if (debug) hitCount++;
+        return data.value;
+      } else {
+        // free some space
+        if (debug) missCount++;
+        size--;
+        delete cache[key];
+      }
+    } else if (debug) {
+      missCount++;
+    }
+    return null;
+  };
 
-exports.hits = function() {
-  return hitCount;
-};
+  store.size = function() {
+    return size;
+  };
 
-exports.misses = function() {
-  return missCount;
-};
+  store.memsize = function() {
+    var size = 0,
+      key;
+    for (key in cache) {
+      size++;
+    }
+    return size;
+  };
 
-exports.keys = function() {
-  return Object.keys(cache);
-};
+  store.debug = function(bool) {
+    debug = bool;
+  };
+
+  store.hits = function() {
+    return hitCount;
+  };
+
+  store.misses = function() {
+    return missCount;
+  };
+
+  store.keys = function() {
+    return Object.keys(cache);
+  };
+
+  return store;
+}
+
+module.exports = createStore();
+module.exports.createStore = createStore;
